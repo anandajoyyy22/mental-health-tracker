ca692ba33cce1607ec3b774d76888bef13e3dbfe
anandajoyyy22 <ananda.joy@ui.ac.id>
2024-09-04 13:08:09 +0700

    update html dan models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
-# Create your models here.
-#dcdc#
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    date_added = models.DateField(auto_now_add=True)
+    price = models.IntegerField()
+    description = models.TextField()
