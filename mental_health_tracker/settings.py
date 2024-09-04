ea24fbf189086bc91c347e09345ba5aaeba59316
anandajoyyy22 <158552130+anandajoyyy22@users.noreply.github.com>
2024-09-04 12:25:41 +0700

    Update settings.py

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://pbp.cs.ui.ac.id/ananda.joy/me
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
