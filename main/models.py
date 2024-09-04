cac576c3f7eb9252a78eef3124889f229fbe8c3c
anandajoyyy22 <ananda.joy@ui.ac.id>
2024-09-04 15:19:37 +0700

    update bang

--- main/models.py
+++ main/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
-    description = models.TextField()
+class MoodEntry(models.Model):
+    mood = models.CharField(max_length=255)
+    time = models.DateField(auto_now_add=True)
+    feelings = models.TextField()
+    mood_intensity = models.IntegerField()
+
+    @property
+    def is_mood_strong(self):
+        return self.mood_intensity > 5
